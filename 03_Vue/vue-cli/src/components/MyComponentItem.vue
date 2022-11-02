c897530cb10e888f60107b8967701ffbe297f07e
hojun0318 <jaden8994@gmail.com>
2022-11-02 17:22:54 +0900

    Vue

---
+++ 03_Vue/vue-cli/src/components/MyComponentItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h3>나는 MyComponent의 하위 컴포넌트</h3>
+    <p>{{ stat }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyComponentItem',
+
+}
+</script>
+
+<style>
+
+</style>
