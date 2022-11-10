f55c472aac878e23936c90b720b7b10c84556b7f
hojun0318 <jaden8994@gmail.com>
2022-11-10 17:51:53 +0900

    221110_onsil

---
+++ 221110_onsil/vue_ws_3_2/src/views/TheLotto.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>로또</h1>
+    <button @click="getLuckyNums">Get Lucky Numbers</button>
+    <p>{{ selectedLuckyNums }}</p>
+  </div>
+</template>
+
+<script>
+  import _ from 'lodash'
+
+  export default {
+    name: 'TheLotto',
+    data: function () {
+      return {
+        sampleNums: [],
+        selectedLuckyNums: [],
+      }
+    },
+    methods: {
+      getLuckyNums: function () {
+        const numbers = _.range(1, 46)
+        this.sampleNums = _.sampleSize(numbers, this.$route.params.lottoNum)
+        this.selectedLuckyNums = _.sortBy(this.sampleNums)
+      },
+    },
+  }
+</script>
---
+++ 221110_onsil/vue_ws_3_2/src/views/TheLunch.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>점심메뉴</h1>
+    <button @click="pickOneInLunchMenu">Pick Lunch</button>
+    <p>{{ selectedLunchMenu }}</p>
+    <br />
+    <h2>로또를 뽑아보자</h2>
+    <button @click="pickLottoNum">Pick Lotto</button>
+  </div>
+</template>
+
+<script>
+  import _ from 'lodash'
+
+  export default {
+    name: 'TheLunch',
+    data: function () {
+      return {
+        lunch: ['국밥', '짜장면', '햄버거'],
+        selectedLunchMenu: null,
+      }
+    },
+    methods: {
+      pickOneInLunchMenu: function () {
+        this.selectedLunchMenu = _.sample(this.lunch)
+      },
+      pickLottoNum: function () {
+        this.$router.push({ name: 'TheLotto', params: { lottoNum: 6 } })
+      },
+    },
+  }
+</script>
+
+<style></style>
